95b22c92de09acf16b293a587a2d09e88b2b59cf
runaway-42 <93150718+runaway-42@users.noreply.github.com>
2021-10-25 11:03:16 -0600

    Add files via upload

---
+++ RPG.py
@@ -0,0 +1,15 @@
+'''
+Brooke Farrell
+Kevin Perez
+
+This program will allow you to build a fighter by choosing three attributes,
+and you will test your fighter against a randomly generated fighter that
+the computer picks.
+
+Honor Code:
+
+we spent
+'''
+import random
+
+keep_going = y
